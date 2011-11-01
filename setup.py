0c789e6cb2832f1a414f64add7415e82736807eb
prior <prior@cracklabs.com>
2011-11-01 18:48:29 -0400

    force versions and bump to latest

--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from distutils.core import setup
 
 setup(
     name='cynq',
-    version='1.1',
+    version='1.1.1',
     description='A data synchronizer',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
-    url='',
+    url='https://github.com/prior/cynq',
     packages=['cynq'],
     install_requires=[
-        'nose',
-        'unittest2',
-        'sanetime==3.0.1'],
-    dependency_links = [
-        'http://github.com/prior/sanetime/tarball/v3.0.1#egg=sanetime-3.0.1'],
-    platforms=["any"] )
+        'nose==1.1.2',
+        'unittest2==0.5.1',
+        'sanetime==3.0.3'],
+    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.0.3'],
+)
 
