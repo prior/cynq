5b1a57e9e8e80e722c22452c9ae455ec009833b8
prior <prior@cracklabs.com>
2011-12-06 23:25:10 -0500

    add download url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/cynq',
+    download_url='https://github.com/prior/cynq/tarball/v2.3',
     packages=['cynq'],
     install_requires=[
         'nose==1.1.2',
