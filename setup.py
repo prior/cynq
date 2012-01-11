86ed0383e09ee1e6fba9dac13d305238f69cef3e
prior <prior@cracklabs.com>
2012-01-11 18:25:42 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='cynq',
-    version='2.3.2',
+    version='2.3.3',
     description='A data synchronizer',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/cynq',
-    download_url='https://github.com/prior/cynq/tarball/v2.3.2',
+    download_url='https://github.com/prior/cynq/tarball/v2.3.3',
     packages=['cynq'],
     install_requires=[
         'nose==1.1.2',
